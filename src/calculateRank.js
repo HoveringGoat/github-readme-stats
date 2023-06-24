a0d9f66fa33a1d0471c52d4d81b09e8d1acf3522
Spencer Fisher <Fisher.spencer@gmail.com>
2023-06-24 15:22:47 -0700

    fix weight calc

--- src/calculateRank.js
+++ src/calculateRank.js
@@ -34,11 +34,11 @@ function calculateRank({
   const PRS_MEDIAN = 50,
     PRS_WEIGHT = 3;
   const ISSUES_MEDIAN = 25,
-    ISSUES_WEIGHT = 1;
+    ISSUES_WEIGHT = 0;
   const STARS_MEDIAN = 50,
-    STARS_WEIGHT = 4;
+    STARS_WEIGHT = 0;
   const FOLLOWERS_MEDIAN = 10,
-    FOLLOWERS_WEIGHT = 1;
+    FOLLOWERS_WEIGHT = 0;
 
   const TOTAL_WEIGHT =
     COMMITS_WEIGHT +
